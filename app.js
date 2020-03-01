d836738a9aeab4699ce61eead313077b59618dfa
Asser Hamad <asserhamad@gmail.com>
2020-03-02 00:59:34 +0200

    yarab

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
