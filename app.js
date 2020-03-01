4c7da6131de110627c4419e6f1ed6a206ed3d3b9
Asser Hamad <asserhamad@gmail.com>
2020-03-02 01:12:51 +0200

    yarab

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
